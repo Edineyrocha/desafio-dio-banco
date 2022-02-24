b7e2858f1869ef592beeaa5da5b9d0fca10b154a
Edineyrocha <94132960+Edineyrocha@users.noreply.github.com>
2022-02-23 21:54:20 -0300

    desafio bootcamp gft Banco

---
+++ desafio-dio-banco/src/pacotebanco/ContaCorrente.java
@@ -0,0 +1,24 @@
+package pacotebanco;
+
+public class ContaCorrente {
+
+	private int agencia;
+	private int conta;
+	private double saldo;
+	
+	
+	
+	
+
+	public void sacar() {
+
+	}
+
+	public void transferir() {
+
+	}
+
+	public void depositar() {
+
+	}
+}
---
+++ desafio-dio-banco/src/pacotebanco/ContaPoupanca.java
@@ -0,0 +1,28 @@
+package pacotebanco;
+
+public class ContaPoupanca {
+	
+	private int agencia;
+	private int conta;
+	private double saldo;
+	
+	
+	
+	
+
+	public void sacar() {
+
+	}
+
+	public void transferir() {
+
+	}
+
+	public void depositar() {
+
+	}
+	
+	
+	
+	
+}
